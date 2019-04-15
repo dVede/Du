18121f7dc8e1edb6bd43911fb45e9cfc084869ea
dVede <dvede123@yandex.ru>
2019-04-16 00:23:52 +0300

    gitignore and pom.xml fix

--- src/main/java/du/DuMethods.java
+++ src/main/java/du/DuMethods.java
@@ -17,9 +17,9 @@ public class DuMethods {
     private final boolean useUnit;
 
     /**
-     * @param useFormat <b>true</b> если найден флаг <b>-h</b>
+     * @param useFormat        <b>true</b> если найден флаг <b>-h</b>
      * @param displayTotalSize <b>true</b> если найден флаг <b>-c</b>
-     * @param useUnit <b>true</b> если найден флаг <b>--si</b>
+     * @param useUnit          <b>true</b> если найден флаг <b>--si</b>
      */
     public DuMethods(boolean useFormat, boolean displayTotalSize, boolean useUnit) {
 
@@ -30,10 +30,11 @@ public class DuMethods {
 
     /**
      * Нахождение размера
+     *
      * @param path Путь к файлу
      * @return Суммарный размер всех файлов
      * @throws IOException Выкидывется в тех случаях, когда не удалось найти
-     * размер файла.
+     *                     размер файла.
      */
     public long fileSize(Path path) throws IOException {
         ArrayList<Path> paths = new ArrayList<>(Collections.singletonList(path));
@@ -49,8 +50,7 @@ public class DuMethods {
                         toAddPaths.add(directoryFile);
                     }
                     directoryFiles.close();
-                }
-                else {
+                } else {
                     totalSize += Files.size(next);
                 }
                 iterator.remove();
@@ -65,16 +65,15 @@ public class DuMethods {
 
     /**
      * Получение конечного результата
+     *
      * @param filePaths Файлы, размеры которыз нужно найти
      * @return Сообщение для вывода
      * @throws IOException Выкидывается в том случае, когда не удается определить размер
-     * @throws MyException Выкидывается в том случае, когда путь файла не найден или при
-     * недостаточном количестве аргументов
      */
 
-    public List<String> answer(String[] filePaths) throws IOException, MyException{
+    public List<String> answer(String[] filePaths) throws IOException {
         if (filePaths == null) {
-            throw new MyException("Не введены аргументы");
+            System.exit(1);
         }
         List<String> result = new ArrayList<>();
         long summarySize = 0;
@@ -84,18 +83,15 @@ public class DuMethods {
             if (!path.isAbsolute()) {
                 path = Paths.get(System.getProperty("user.dir"), filePath);
             }
-            if (Files.notExists(path)) {
-                throw new MyException("Ошибка, файл " + path + " не существует");
-            }
+            if (Files.notExists(path))
+                System.exit(1);
             long size = fileSize(path);
             if (displayTotalSize) {
                 summarySize += size;
-            }
-            else {
+            } else {
                 if (useFormat) {
                     result.add(appendFormat("Размер " + filePath + " равен ", size, unit));
-                }
-                else {
+                } else {
                     result.add("Размер " + filePath + " равен " + size / unit);
                 }
             }
@@ -103,8 +99,7 @@ public class DuMethods {
         if (displayTotalSize) {
             if (useFormat) {
                 result.add(appendFormat("Суммарный размер равен ", summarySize, unit));
-            }
-            else {
+            } else {
                 result.add("Суммарный размер равен " + summarySize / unit);
             }
         }
@@ -113,6 +108,7 @@ public class DuMethods {
 
     /**
      * Формат, в котором выведится ответ
+     *
      * @param text Текст, который будет содержаться в результате
      * @param size Размер файлов, который будет содержаться в результате
      * @param unit Единица измерения, которая будет содержаться в результате
@@ -120,7 +116,7 @@ public class DuMethods {
      */
     private String appendFormat(String text, long size, int unit) {
         int form = 0;
-        while (size / unit > 0) {
+        while ((size / unit > 0) && (form < 3)) {
             size /= unit;
             form++;
         }
@@ -133,6 +129,5 @@ public class DuMethods {
         }
     }
 }
-
 
 
--- src/test/java/du/DuParseTest.java
+++ src/test/java/du/DuParseTest.java
@@ -4,19 +4,27 @@ import du.DuParse;
 import du.DuParse.*;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.After;
 import org.junit.jupiter.api.*;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 public class DuParseTest {
     private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private PrintStream originalOut = System.out;
     private ByteArrayInputStream inContent;
+
+    private String resourcesDirectory = new File("src/test/resources").getAbsolutePath();
+
+    private Path dir2 = Paths.get("src","test","resources","testDirectory");
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
     @Before
     public void duStream() {
@@ -28,30 +36,32 @@ public class DuParseTest {
 
     @Test
     public void consoleTest() throws IOException {
-    String[] args1 = {"-c", "-h", "C:\\Users\\пользователь\\IdeaProjects\\du\\out\\artifacts\\du_jar\\1.txt",
-            "C:\\Users\\пользователь\\IdeaProjects\\du\\out\\artifacts\\du_jar\\testDirectory"};
-    DuParse.main(args1);
-    String excepted = "Суммарный размер равен 499 KB\r\n" ;
-    String actual = outContent.toString();
+        Path dir1 = Paths.get("src","test","resources","1.txt");
+        Path dir2 = Paths.get("src","test","resources","testDirectory");
+        String[] args1 = {"-c", "-h", dir1.toString(), dir2.toString()};
+        DuParse.main(args1);
+        String excepted = "Суммарный размер равен 499 KB\r\n" ;
+        String actual = outContent.toString();
         Assertions.assertEquals(excepted, actual);
     }
+
     @Test
-    public void absoluteTest() throws IOException {
-        String[] args1 = {"-c", "-h", "1.txt",
-                "C:\\Users\\пользователь\\IdeaProjects\\du\\out\\artifacts\\du_jar\\testDirectory"};
+    public void absoluteTest() throws IOException { ;
+        String[] args1 = {"-c", "-h", "1.txt", dir2.toString()};
         DuParse.main(args1);
         String excepted = "Суммарный размер равен 413 KB\r\n" ;
         String actual = outContent.toString();
         Assertions.assertEquals(excepted, actual);
     }
+
     @Test
     public void noArgumentTest() throws IOException {
         String[] args1 = {"-c", "-h"};
+        exit.expectSystemExit();
+        System.exit(1);
         DuParse.main(args1);
-        String excepted = "" ;
-        String actual = outContent.toString();
-        Assertions.assertEquals(excepted, actual);
     }
+
     @Test
     public void notCorrectTest() throws IOException {
         String[] args1 = {"-correct", "-h"};
@@ -60,44 +70,45 @@ public class DuParseTest {
         String actual = outContent.toString();
         Assertions.assertEquals(excepted, actual);
     }
+
     @Test
     public void fileNotFound() throws IOException {
-        String[] args1 = {"-c", "-h", "2.txt",
-                "C:\\Users\\пользователь\\IdeaProjects\\du\\out\\artifacts\\du_jar\\testDirectory"};
+        String[] args1 = {"-c", "-h", "2.txt", "testDirectory"};
+        exit.expectSystemExit();
+        System.exit(1);
         DuParse.main(args1);
-        String excepted = "" ;
-        String actual = outContent.toString();
-        Assertions.assertEquals(excepted, actual);
     }
+
     @Test
     public void withNoSummarySize() throws IOException {
-        String[] args1 = {"-h", "--si","1.txt",
-                "C:\\Users\\пользователь\\IdeaProjects\\du\\out\\artifacts\\du_jar\\testDirectory"};
+        String[] args1 = {"-h", "--si", "1.txt", dir2.toString()};
         DuParse.main(args1);
         String excepted = "Размер 1.txt равен 12 KB\r\n" +
-                "Размер C:\\Users\\пользователь\\IdeaProjects\\du\\out\\artifacts\\du_jar\\testDirectory равен 410 KB\r\n" ;
+                "Размер " + dir2.toString() + " равен 410 KB\r\n";
         String actual = outContent.toString();
         Assertions.assertEquals(excepted, actual);
     }
+
     @Test
     public void onlyUseUnit() throws IOException {
-        String[] args1 = {"--si","1.txt",
-                "C:\\Users\\пользователь\\IdeaProjects\\du\\out\\artifacts\\du_jar\\testDirectory"};
+        String[] args1 = {"--si", "1.txt", dir2.toString()};
         DuParse.main(args1);
         String excepted = "Размер 1.txt равен 12\r\n" +
-                "Размер C:\\Users\\пользователь\\IdeaProjects\\du\\out\\artifacts\\du_jar\\testDirectory равен 410\r\n" ;
+                "Размер " + dir2.toString() + " равен 410\r\n" ;
         String actual = outContent.toString();
         Assertions.assertEquals(excepted, actual);
     }
+
     @Test
     public void withNoUseFormat() throws IOException {
         String[] args1 = {"-c", "--si", "1.txt",
-                "C:\\Users\\пользователь\\IdeaProjects\\du\\out\\artifacts\\du_jar\\testDirectory"};
+                dir2.toString()};
         DuParse.main(args1);
         String excepted = "Суммарный размер равен 423\r\n";
         String actual = outContent.toString();
         Assertions.assertEquals(excepted, actual);
     }
+
     @After
     public void restoreStreams(){
         System.setOut(originalOut);
